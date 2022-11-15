7295fe56445078aa644bc869189753bea2e6a953
saeinlee <gitsaein@gmail.com>
2022-11-16 08:18:16 +0900

    webFlux webSocket setting

--- src/main/java/com/kafkaSocket/chat/ChatApplication.java
+++ src/main/java/com/kafkaSocket/chat/ChatApplication.java
@@ -2,17 +2,8 @@ package com.kafkaSocket.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import com.kafkaSocket.chat.config.MessageHandler;
 
 @EnableMongoAuditing
 @EnableReactiveMongoRepositories
